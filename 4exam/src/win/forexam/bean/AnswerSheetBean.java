eee472f8e99c4668a3a41a74dde5d6b3f3f085c3
xianfeng <liuxf0102@163.com>
2016-03-23 18:04:07 +0800

    init

---
+++ 4exam/src/win/forexam/bean/AnswerSheetBean.java
@@ -0,0 +1,24 @@
+package win.forexam.bean;
+
+import java.util.List;
+
+import win.forexam.model.AnswerSheet;
+import win.forexam.model.AnswerSheetQuestion;
+
+public class AnswerSheetBean {
+	private AnswerSheet answerSheet;
+	private List<AnswerSheetQuestion> listQuestion;
+	public AnswerSheet getAnswerSheet() {
+		return answerSheet;
+	}
+	public void setAnswerSheet(AnswerSheet answerSheet) {
+		this.answerSheet = answerSheet;
+	}
+	public List<AnswerSheetQuestion> getListQuestion() {
+		return listQuestion;
+	}
+	public void setListQuestion(List<AnswerSheetQuestion> listQuestion) {
+		this.listQuestion = listQuestion;
+	}
+	
+}
